0fed499198db0efc977149464cb5ab139c6bd22d
Michael Jones <61482297+mjones12209@users.noreply.github.com>
2022-12-14 16:09:35 -0500

    added more text

--- testFile.js
+++ testFile.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const testFile = () => {
   return (
-    <div>testFile - with more text -- and added more text</div>
+    <>
+      <div>testFile - with more text -- and added more text</div>
+      <div>another div</div>
+    </>
   )
 }
 
