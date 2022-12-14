b70b71d5b88b19c2a670d1cccd3a51072de8d616
Michael Jones <61482297+mjones12209@users.noreply.github.com>
2022-12-14 14:21:25 -0500

    add github workflows

---
+++ .github/scripts/update_children_branches.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#define children branch names
+CHILDREN_BRANCHES=('a' 'b')
+
+
+#basic setup for git with bot naming
+git config user.name "Automerge Bot"
+git config user.email "bot@example.com"
+git config pull.rebase false 
+
+
+#are we going to have dev work flows for each ?
+
+
+#loop through the children branches and merge with master
+for i in ${CHILDREN_BRANCHES[@]};
+do
+    git checkout $i
+    git merge master
+done
